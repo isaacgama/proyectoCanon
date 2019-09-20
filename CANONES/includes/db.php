3d0dc11839a0857e19c3cdc68f23332af55d13c1
gama96 <00266230@red.unid.mx>
2019-09-20 17:22:30 -0500

    new
    se agrego usuario y cañon

---
+++ CANONES/includes/db.php
@@ -0,0 +1,15 @@
+<?php
+// Using Medoo namespace
+require 'Medoo.php';
+use Medoo\Medoo;
+// $server = (_SERVER['HTTP_HOST']) == "localhost") ? "smoothoperators.com.mx" : "smoothoperators.com.mx";
+// Initialize en el segundo local host y el tercero va ir el dominip
+$db = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'proyecto-desarrollo',
+    'server' => 'smoothoperators.com.mx',
+    'username' => 'remote',
+    'password' => 'D7AC3D58A7318'
+]); 
+//print_r($db);
+ ?>
